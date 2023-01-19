67daead66548efc86e411e4fdd7489a7a41bf2e4
kjmfum <kjmfum@yahoo.co.uk>
2023-01-19 04:58:15 +0000

    update getTodos logic

--- course-04/project/c4-final-project-starter-code/backend/src/lambda/http/getTodos.ts
+++ course-04/project/c4-final-project-starter-code/backend/src/lambda/http/getTodos.ts
@@ -6,14 +6,50 @@ import { cors } from 'middy/middlewares'
 
 import { getTodosForUser as getTodosForUser } from '../../businessLogic/todos'
 import { getUserId } from '../utils';
+import * as AWS from 'aws-sdk'
 
 // TODO: Get all TODO items for a current user
+
+const docClient = new AWS.DynamoDB.DocumentClient()
+
+const todosTable = process.env.todosTable
+const todosIndexName = process.env.TODOS_CREATED_AT_INDEX
+
+
 export const handler = middy(
   async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
     // Write your code here
-    const todos = '...'
+    // const userId = getUserId(event)
+    // const todos = await getTodosForUser(userId)
 
-    return undefined
+    const todos = getTodosForUser(event)
+
+    return {
+      statusCode: 200,
+      body: JSON.stringify({
+        items: todos
+      })
+    }
+
+  //   async function  getTodosForUser(userId: string){
+  //     const result = await docClient.query(
+  //       {
+  //         TableName: todosTable,
+  //         IndexName: todosIndexName,
+  //         KeyConditionExpression: 'userId = :userId',
+  //         ExpressionAttributeValues: {
+  //           ':userId' : userId
+  //         },
+  //         ScanIndexForward:false
+  //       }
+  //     ).promise()
+
+  //     return result.Items;
+  // }
+  },
+ 
+  
+  )
 
 handler.use(
   cors({
